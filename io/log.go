778bc84fba0971ef0ebe3d9a18ab5145ae1a0f71
THINKPAD\THINK <1650665999@qq.com>
2021-05-21 10:25:49 +0800

    log

---
+++ io/log.go
@@ -0,0 +1,22 @@
+package io
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+// WriteLog 将日志内容 text 写入到 file 中
+func WriteLog(file string, text string, prefix string) error {
+	logFile, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	logger := GetLogger(logFile, prefix, log.Llongfile|log.Lmicroseconds|log.Ldate)
+	logger.Println(text)
+	return nil
+}
+
+func GetLogger(out io.Writer, prefix string, flag int) *log.Logger {
+	return log.New(out, prefix, flag)
+}
